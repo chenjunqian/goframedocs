99d31e7b75cb38c4910721f2473ef94d2c6019db
chenjunqian <chenjunqian0810@foxmail.com>
2024-07-01 00:48:22 +0800

    update: feature description

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
-import { Boxes, Component, Github, SquareArrowRight, Unplug } from "lucide-react";
+import { BicepsFlexed, Bird, BookOpenText, Boxes, BugOff, Code, Component, DatabaseZap, FileTerminal, Github, ListCollapse, MessageSquareCode, Rocket, SquareArrowRight, Telescope, Unplug } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -12,13 +12,13 @@ export default function Home() {
                 </div>
             </div>
             <div className="w-full flex justify-center mt-20">
-                <div className="text-4xl md:w-1/2 w-full flex justify-center text-center">
+                <div className="text-4xl font-bold md:w-1/2 w-full flex justify-center text-center">
                     Goframe Web Framework
                 </div>
             </div>
             <div className="w-full flex justify-center mt-6">
                 <div className="text-base text-center md:w-1/3 w-full flex justify-center">
-                    Goframe is a modular, high-performance web framework and toolkit for Go that extends the standard library with a rich set of components
+                    Goframe s a modular, powerful, high-performance and enterprise-class application development framework of Golang.
                 </div>
             </div>
             <div className="w-full flex justify-center mt-4">
@@ -34,30 +34,71 @@ export default function Home() {
                 </Link>
             </div>
 
-            <div className="w-full flex justify-center mt-36">
-                <div className="columns-3 gap-8">
-                    <div className="w-full">
-                        <div className="flex justify-center text-2xl items-center">
-                            <Boxes className="mr-4" /> Modular
-                        </div>
-                        <div className="text-base text-center">
-                            Modular, loosely coupled design.
-                        </div>
-                    </div>
-                    <div className="w-full">
-                        <div className="flex justify-center text-2xl items-center">
-                            <Component className="mr-4" /> Rich Component
-                        </div>
-                        <div className="text-base text-center">
-                            Rich components, ready-to-use out of the box.
+            <div className="w-full flex justify-center mt-20">
+                <div className="text-4xl font-bold md:w-1/2 w-full flex justify-center text-center">
+                    What is it?
+                </div>
+            </div>
+            <div className="w-full flex justify-center md:text-2xl text-sm md:p-6 p-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div className="w-full min-h-8 mt-4">
+                        <div className="flex justify-start items-center">
+                            <Boxes className="mr-4" /> Modular, loosely coupled design
                         </div>
                     </div>
-                    <div className="w-full">
-                        <div className="flex justify-center text-2xl items-center">
-                            <Unplug className="mr-4" /> Interface-based
+                    <div className="w-full min-h-8 mt-4">
+                        <div className="flex justify-start items-center">
+                            <Component className="mr-4" /> Rich components, out-of-the-box
                         </div>
-                        <div className="text-base text-center">
-                            Interface-oriented, highly extensible design.
+                    </div>
+                    <div className="w-full min-h-8 mt-4">
+                        <div className="flex justify-start items-center">
+                            <Code className="mr-4" /> Codegen for efficiency
+                        </div>
+                    </div>
+                    <div className="w-full min-h-8 mt-4">
+                        <div className="flex justify-start items-center">
+                            <Bird className="mr-4" /> Simple and easy to use
+                        </div>
+                    </div>
+                    <div className="w-full min-h-8 mt-4">
+                        <div className="flex justify-start items-center">
+                            <Unplug className="mr-4" /> Interface designed components, high scalability
+                        </div>
+                    </div>
+                    <div className="w-full min-h-8 mt-4">
+                        <div className="flex justify-start items-center">
+                            <BugOff className="mr-4" /> Fully supported tracing and error stack feature
+                        </div>
+                    </div>
+                    <div className="w-full min-h-8 mt-4">
+                        <div className="flex justify-start items-center">
+                            <DatabaseZap className="mr-4" /> Specially developed and powerful ORM component
+                        </div>
+                    </div>
+                    <div className="w-full min-h-8 mt-4">
+                        <div className="flex justify-start items-center">
+                            <BicepsFlexed className="mr-4" /> Robust engineering design specifications
+                        </div>
+                    </div>
+                    <div className="w-full min-h-8 mt-4">
+                        <div className="flex justify-start items-center">
+                            <FileTerminal className="mr-4" /> Convenient development CLI tool provide
+                        </div>
+                    </div>
+                    <div className="w-full min-h-8 mt-4">
+                        <div className="flex justify-start items-center">
+                            <Telescope className="mr-4" /> OpenTelemetry observability features support
+                        </div>
+                    </div>
+                    <div className="w-full min-h-8 mt-4">
+                        <div className="flex justify-start items-center">
+                            <BookOpenText className="mr-4" /> OpenAPIV3 documentation generating, automatically
+                        </div>
+                    </div>
+                    <div className="w-full min-h-8 mt-4">
+                        <div className="flex justify-start items-center">
+                            <ListCollapse className="mr-4" /> Much, much more...ready to explore?
                         </div>
                     </div>
                 </div>

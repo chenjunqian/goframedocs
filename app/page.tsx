099b038218711b2c54a37da1785a15be7f730b7b
Chenjunqian <chenjunqian0810@foxmail.com>
2024-06-30 09:44:12 +0000

    update: responsive

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,17 @@ export default function Home() {
     return (
         <div className="min-h-screen w-full">
             <div className="w-full flex justify-center mt-20">
-                <div className="text-4xl w-1/2 flex justify-center">
+                <div className="text-4xl md:w-1/2 w-full flex justify-center">
                     <Image src={"/gf-logo.png"} alt={"logo"} width={256} height={256} />
                 </div>
             </div>
             <div className="w-full flex justify-center mt-20">
-                <div className="text-4xl w-1/2 flex justify-center">
+                <div className="text-4xl md:w-1/2 w-full flex justify-center">
                     Goframe Web Framework
                 </div>
             </div>
             <div className="w-full flex justify-center mt-6">
-                <div className="text-base text-center w-1/3 flex justify-center">
+                <div className="text-base text-center md:w-1/3 w-full flex justify-center">
                     Goframe is a modular, high-performance web framework and toolkit for Go that extends the standard library with a rich set of components
                 </div>
             </div>
@@ -32,6 +32,23 @@ export default function Home() {
                         Github <Github className="ml-2" />
                     </Button>
                 </Link>
+            </div>
+
+            <div className="w-full flex justify-center mt-36">
+                <div className="w-full flex md:w-2/3 justify-center">
+                    <div className="columns-3 gap-8">
+                        <div>
+                            Modular
+                        </div>
+                        <div>
+                           Rich Component 
+                        </div>
+                        <div>
+                            Interface-based, highly extensible design
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     );

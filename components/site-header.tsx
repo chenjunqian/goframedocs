b1fc610f0f6e3b4340c091e324cddaf2e8f58b14
chenjunqian <chenjunqian0810@foxmail.com>
2024-07-03 00:22:45 +0800

    update: refine header and footer with daisyui

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,8 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+import { siteConfig } from "../config/site";
+import { Github } from "lucide-react";
 
 export function SiteHeader() {
 
     return (
-        <header className="bg-background sticky top-0 z-40 w-full border-b">
+        <header className="z-40 sticky w-full right-4 left-4">
+            <div className="navbar bg-base-200">
+                <div className="navbar-start">
+                    <Image src="/gf-logo.png" alt="logo" width={32} height={32} className="ml-6" />
+                    <a className="btn btn-ghost text-xl" href="/">Goframe</a>
+                </div>
+                <div className="navbar-center flex">
+                </div>
+                <div className="navbar-end">
+                    <ul className="px-1 flex items-center">
+                        <li className="mr-4">
+                            <Link href="/docs">
+                                <button className="btn btn-link text-lg">Document</button>
+                            </Link>
+                        </li>
+                        <li className="mr-4">
+                            <Link href={siteConfig.links.github} target="_blank">
+                                <button className="btn btn-sm btn-circle">
+                                    <Github className="w-5 h-5" />
+                                </button>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </header>
     )
 }

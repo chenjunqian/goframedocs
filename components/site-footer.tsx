b837c51b9d016bca8a5b6f50ded147e6ea4b8fa6
chenjunqian <chenjunqian0810@foxmail.com>
2024-07-03 23:44:32 +0800

    update: refine

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -3,7 +3,7 @@ import { siteConfig } from "../config/site";
 
 export default function SiteFooter() {
     return (
-        <footer className="bg-base-200 md:sticky top-0 z-40 w-full mt-9">
+        <footer className="bg-base-200 md:sticky top-0 z-40 w-full">
             <div className="container flex flex-col items-center justify-between py-10 gap-4 md:h-24 md:flex-row">
                 <p className="text-sm text-muted-foreground ml-10">
                     Copyright © 2020 - {new Date().getFullYear()} <a href={siteConfig.links.github} target="_blank">Goframe</a>. All rights reserved.

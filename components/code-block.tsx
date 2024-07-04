558a1da09814c8b5a920eb057f80892dfb5ae514
chenjunqian <chenjunqian0810@foxmail.com>
2024-07-04 22:50:47 +0800

    feat: add code block component

---
+++ components/code-block.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { Clipboard, ClipboardCopy } from 'lucide-react';
+import React, { useState } from 'react';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+function CodeBlock({ language, code }: { language: string, code: string }) {
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(code);
+        setCopied(true);
+        setTimeout(() => {
+            setCopied(false);
+        }, 2000);
+    };
+
+    return (
+        <div className="relative">
+            <SyntaxHighlighter language={language} style={atomDark}>
+                {code}
+            </SyntaxHighlighter>
+            <button className="absolute top-0 right-0 px-2 py-1 btn btn-ghost btn-sm" onClick={handleCopy}>
+                {
+                    copied ? <ClipboardCopy className="w-5 h-5" /> : <Clipboard className="w-5 h-5" />
+                }
+            </button>
+        </div>
+    );
+}
+
+export default CodeBlock;

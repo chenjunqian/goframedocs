d4cc1aa63fbb45633b36b64dfb3096131e21e6c7
chenjunqian <chenjunqian0810@foxmail.com>
2024-07-07 23:39:18 +0800

    feat: add anchor for heading

--- components/docs-markdown-viewer.tsx
+++ components/docs-markdown-viewer.tsx
@@ -23,8 +23,60 @@ export function DocsMarkdownViewer({ children }: { children: string }) {
                             {children}
                         </code>
                     )
-                }
+                },
+                h1(props) {
+                    const children = props.children
+                    return <Heading level={1} children={children} />
+                },
+                h2(props) {
+                    const children = props.children
+                    return <Heading level={2} children={children} />
+                },
+                h3(props) {
+                    const children = props.children
+                    return <Heading level={3} children={children} />
+                },
             }}
         />
     )
+}
+
+type HeadingResolverProps = {
+    level: number;
+    children: React.ReactNode;
+};
+
+const Heading = ({ level, children }: HeadingResolverProps) => {
+    // Access actual (string) value of heading
+    const heading = children?.valueOf();
+
+    // If we have a heading, make it lower case
+    let anchor = typeof heading === 'string' ? heading.toLowerCase() : '';
+
+    // Clean anchor (replace special characters whitespaces).
+    // Alternatively, use encodeURIComponent() if you don't care about
+    // pretty anchor links
+    anchor = anchor.replace(/[^a-zA-Z0-9 ]/g, '');
+    anchor = anchor.replace(/ /g, '-');
+
+    // Utility
+    const container = (children: React.ReactNode) => {
+        return (
+            <a id={anchor} href={`#${anchor}`} style={{textDecoration: 'none'}}>
+                <span>{children}</span>
+            </a>
+        )
+    }
+
+    switch (level) {
+        case 1:
+            return <h1>{container(children)}</h1>;
+        case 2:
+            return <h2>{container(children)}</h2>;
+        case 3:
+            return <h3>{container(children)}</h3>;
+
+        default:
+            return <h6>{container(children)}</h6>;
+    }
 }

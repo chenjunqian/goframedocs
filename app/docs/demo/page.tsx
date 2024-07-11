26f598e031d09d685e1b16c01fa814195f4883bc
chenjunqian <chenjunqian0810@foxmail.com>
2024-07-11 23:29:08 +0800

    update: add demo page

--- app/docs/demo/page.tsx
+++ app/docs/demo/page.tsx
@@ -5,13 +5,13 @@ import { promises as fs } from "fs";
 
 export default async function Page() {
 
-    const installationMD = await fs.readFile(process.cwd() + "/docs/quick-start/configuration.md", "utf-8");
+    const installationMD = await fs.readFile(process.cwd() + "/docs/quick-start/demo.md", "utf-8");
 
     return (
         <DocsDrawer>
             <div className="w-full pl-16 pr-16 mt-9">
                 <DocsMarkdownViewer children={installationMD} />
-                <DocsPreNextBtns preName="Instance Startup" prePath="/docs/instance-startup" nextName="Demo" nextPath="/docs/demo" />
+                <DocsPreNextBtns preName="Configuration" prePath="/docs/configuration" nextName="Framework Design" nextPath="/docs/framework-design" />
             </div>
         </DocsDrawer>
     )
---
+++ docs/quick-start/demo.md
@@ -0,0 +1,5 @@
+# Demo Project
+
+Example is the best documentation, you can start from a demo project to learn the basic usage of Goframe.
+
+Project Github Link: <https://github.com/gogf/gf-demo-user>
